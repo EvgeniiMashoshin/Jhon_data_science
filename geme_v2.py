03f1331984462fe1dd29afd3abd12533ac3dea65
EvgeniiMashoshin <93088765+EvgeniiMashoshin@users.noreply.github.com>
2021-11-14 21:49:00 +0300

    Игра 2 - "Угадай число"
    Компьютер сам загадывает и сам угадывает число.
    Задача - Написать код где компьютер загадывает целое число от 1 до 100, и нам его нужно угадать. Под «угадать» подразумевается «написать программу, которая угадывает число».
    Условия задачи - Программа должна находит нужное число меньше чем за 20 попыток.

---
+++ geme_v2.py
@@ -0,0 +1,60 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Угадываем число
+    
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+        
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    lower_borber = 1
+    upper_borber = 100
+    predict_number = np.random.randint(1,101)
+
+    while True:
+        count += 1
+    
+        if number > predict_number:
+            lower_borber = predict_number
+        
+        elif number < predict_number:
+            upper_borber = predict_number
+        
+            predict_number = round(lower_borber+upper_borber)/2
+        return count
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+    
+    Args:
+        random_predict ([type]): функция угадывания
+        
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=(10000))
+    
+    for number in random_array:
+        count_ls.append(random_predict(number))
+        
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
+    
+    
+    
